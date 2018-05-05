c30e2a176eb1556dcd229f0651934148050d3e64
Luis Otero <luisote94@gmail.com>
2018-05-04 22:57:06 -0700

    More updates on main app

--- Apps/main.js
+++ Apps/main.js
@@ -1,22 +1,16 @@
 var cart = require("./cartApp/app");
 var payment = require("./paymentApp/app");
-var users = require("./usersApp/app");
-var history = require("./userHistory/app");
-var products = require("./products/app");
+var users = require("./UserApp/app");
+var history = require("./User History/app");
+var products = require("./ProductsApp/app");
 
 cart.use(payment);
 cart.use(users);
 cart.use(history);
 cart.use(products);
 cart.set('port', process.env.PORT || '4000');
+
 cart.listen(cart.get('port'), function() {
   	console.log("Running on port 4000");
 });
-
-/*
-payment.set('port', process.env.PORT || '10000')
-payment.listen(payment.get('port'), function() {
-	
-});
-*/
 
